0a7b121df97e5bf77ef1b95a9aa8f745dcd8b6e1
Byungju Kim <feidfoe@gmail.com>
2019-11-18 14:48:54 +0900

    Update and rename train_cifar10.sh to run_cifar.sh

--- run_cifar.sh
+++ run_cifar.sh
@@ -9,15 +9,17 @@ dataset=CIFAR10
 arch=resnet
 depth=32
 
+TRAIN=true
+EVAL=false
 
+if $TRAIN; then 
 ExpName=${dataset}_${arch}_${depth}
-nvidia-docker run -v `pwd`:`pwd` \
+NV_GPU=${GPUID} nvidia-docker run -v `pwd`:`pwd` \
                   -v ${dataset_path}:`pwd`/data/ \
                   -v ${checkpoint_path}:`pwd`/checkpoints/ \
                   -w `pwd` \
                   --rm -it \
                   --ipc=host \
-                  -e CUDA_VISIBLE_DEVICES=${GPUID} \
                   --name ${ExpName}'_gpu'${GPUID} \
                   feidfoe/pytorch:v.2 \
                   python cifar.py -a ${arch} \
@@ -27,4 +29,23 @@ nvidia-docker run -v `pwd`:`pwd` \
                                   --gamma 0.1 \
                                   --wd 5e-4 \
                                   --checkpoint checkpoints/${ExpName} \
+fi
 
+
+if $EVAL; then
+ExpName=${dataset}_${arch}_${depth}
+CKPT=checkpoints/${ExpName}/checkpoint.pth.tar
+NV_GPU=${GPUID} nvidia-docker run -v `pwd`:`pwd` \
+                  -v ${dataset_path}:`pwd`/data/ \
+                  -v ${checkpoint_path}:`pwd`/checkpoints/ \
+                  -w `pwd` \
+                  --rm -it \
+                  --ipc=host \
+                  --name ${ExpName}'_gpu'${GPUID} \
+                  feidfoe/pytorch:v.2 \
+                  python cifar.py -a ${arch} \
+                                  --depth $depth \
+                                  --resume ${CKPT} \
+                                  --evaluate
+
+fi
